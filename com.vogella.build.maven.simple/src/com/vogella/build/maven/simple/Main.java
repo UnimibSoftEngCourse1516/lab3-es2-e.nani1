28acf9981f8ceb3a866639aee40d745a4cd65f40
Edoardo Nani <e.nani1@campus.unimib.it>
2015-12-02 09:27:53 +0100

    Add main body

--- com.vogella.build.maven.simple/src/com/vogella/build/maven/simple/Main.java
+++ com.vogella.build.maven.simple/src/com/vogella/build/maven/simple/Main.java
@@ -3,7 +3,8 @@ package com.vogella.build.maven.simple;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Maven!");
 
 	}
 
